9f85bc187e4a9d657d1320e2fa3b232558982ef6
Alex Sayers <alex.sayers@gmail.com>
2014-08-27 16:22:04 +0200

    Allow reverting internal state using the time machine

--- todomvc/TodoApp.hs
+++ todomvc/TodoApp.hs
@@ -29,6 +29,7 @@ import           Control.Lens
 import           Control.Monad
 
 import           Data.Foldable   (foldMap)
+import           Data.List       (foldl')
 import           Data.Monoid     ((<>), mempty)
 import qualified Data.Text       as T
 import           Data.Time       (UTCTime)
@@ -360,30 +361,45 @@ todoApp = App
 data TMState state action = TMState
     { _tmsInternalState :: state
     , _tmsActionHistory :: [action]
+      -- ^ List of actions, from earliest to latest
+    , _tmsActiveAction  :: Int
+      -- ^ Index of the current position in the action list. 1-indexed,
+      -- where 0 indicates that the app is in the initial state.
     , _tmsPaused        :: Bool
     }
 
 data TMAction action
     = TogglePauseAppA
+    | RevertAppHistoryA Int
     | InternalA action
     deriving (Eq, Ord, Read, Show)
 
 data TMEventHandler eventHandler
     = TogglePauseAppEH
+    | ActionHistoryItemEH Int
     | InternalEH eventHandler
     deriving (Eq, Ord, Read, Show)
 
 makeLenses ''TMState
 
-applyTMAction :: (a -> s -> s) -> TMAction a -> TMState s a -> TMState s a
-applyTMAction applyInternalAction action state =
+applyTMAction :: s -> (a -> s -> s) -> TMAction a -> TMState s a -> TMState s a
+applyTMAction initialInternalState applyInternalAction action state =
     case action of
       TogglePauseAppA -> over tmsPaused not state
+      RevertAppHistoryA idx ->
+        let history'       = take idx $ view tmsActionHistory state
+            internalState' = foldl' (flip applyInternalAction) initialInternalState history'
+        in set tmsInternalState internalState' $ set tmsActiveAction idx $ state
       InternalA action'
         | view tmsPaused state -> state
         | otherwise            ->
-            over tmsActionHistory (action':) $
-            over tmsInternalState (applyInternalAction action') state
+            let state' = if (_tmsActiveAction state) == length (_tmsActionHistory state)
+                           then state
+                           else over tmsActionHistory (take (_tmsActiveAction state)) state
+            in over tmsActionHistory (++ [action']) $
+            over tmsActiveAction (+ 1) $
+            over tmsInternalState (applyInternalAction action') $
+            state'
 
 renderTM :: Show a => (s -> H.Html eh) -> TMState s a -> H.Html (TMEventHandler eh)
 renderTM renderInternal state = do
@@ -391,28 +407,39 @@ renderTM renderInternal state = do
       H.h1 "Time machine"
       H.div H.! A.class_ "tm-button" H.! H.onEvent TogglePauseAppEH $
         if (_tmsPaused state) then "Resume app" else "Pause app"
-      H.h2 "Events"
-      H.ol $ forM_ (reverse $ _tmsActionHistory state) $ \action ->
-        H.li $ H.toHtml $ show action
+      renderHistoryBrowser
     H.div H.! A.class_ "tm-internal-app" $
       H.mapEventHandlers InternalEH $ renderInternal (view tmsInternalState state)
+  where
+    actionsWithIndices :: [(Int, String)]
+    actionsWithIndices =
+      (0, "Initial state") : (zip [1..] $ map show $ _tmsActionHistory state)
+
+    renderHistoryBrowser = do
+      H.h2 "Events"
+      H.div H.! A.class_ "tm-history-browser" $ do
+        H.ol $ forM_ actionsWithIndices $ \(idx, action) ->
+          H.li H.! H.onEvent (ActionHistoryItemEH idx)
+               H.!? (idx == view tmsActiveAction state, A.class_ "tm-active")
+               $ H.toHtml action
 
 handleTMEvent
     :: (UTCTime -> DOMEvent -> eh -> Maybe a)
     -> UTCTime -> DOMEvent -> TMEventHandler eh -> Maybe (TMAction a)
 handleTMEvent handleInternalEvent utcTime domEvent handler =
-    case handler of
-      TogglePauseAppEH ->
-        case domEvent of
-          OnClick -> Just TogglePauseAppA
-          _       -> Nothing
-      InternalEH handler' ->
+    case (handler, domEvent) of
+      (TogglePauseAppEH, OnClick) -> Just TogglePauseAppA
+      (TogglePauseAppEH, _      ) -> Nothing
+      (ActionHistoryItemEH idx, OnClick) -> Just $ RevertAppHistoryA idx
+      (ActionHistoryItemEH _, _        ) -> Nothing
+      (InternalEH handler', _) ->
         InternalA <$> handleInternalEvent utcTime domEvent handler'
 
 initialTMState :: s -> TMState s a
 initialTMState internalState = TMState
     { _tmsInternalState = internalState
     , _tmsActionHistory = []
+    , _tmsActiveAction  = 0       -- ^ 0 indicates the initial state.
     , _tmsPaused        = False
     }
 
@@ -421,7 +448,7 @@ withTimeMachine
     -> App (TMState s a) (TMAction a) (TMEventHandler eh)
 withTimeMachine internalApp = App
     { appInitialState = initialTMState (appInitialState internalApp)
-    , appApplyAction  = applyTMAction (appApplyAction internalApp)
+    , appApplyAction  = applyTMAction (appInitialState internalApp) (appApplyAction internalApp)
     , appRender       = renderTM (appRender internalApp)
     , appHandleEvent  = handleTMEvent (appHandleEvent internalApp)
     }
