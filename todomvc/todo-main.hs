82de32081af765e07ede0caf6f7da8af53b501ff
Alex Sayers <alex.sayers@gmail.com>
2014-12-12 01:29:30 +0100

    Add the RoutingTest demo app

--- todomvc/todo-main.hs
+++ todomvc/todo-main.hs
@@ -6,6 +6,7 @@ import           Blaze.React
 
 import qualified Blaze.React.Examples.Clock       as Clock
 import qualified Blaze.React.Examples.EventTest   as EventTest
+import qualified Blaze.React.Examples.RoutingTest as RoutingTest
 import qualified Blaze.React.Examples.Todo        as Todo
 import           Blaze.React.Examples.TabbedApps  (namedApp, namedApp', tabbed)
 import           Blaze.React.Examples.TimeMachine (withTimeMachine)
@@ -21,4 +22,5 @@ main = ReactJS.runApp $
       , namedApp "Multi-user TodoMVC" $ withTimeMachine $ withMultiUser $ ignoreWindowActions Todo.app
       , namedApp' "Clock" Clock.app
       , namedApp' "Event test" EventTest.app
+      , namedApp  "Routing test"                $ withTimeMachine RoutingTest.app
       ]
