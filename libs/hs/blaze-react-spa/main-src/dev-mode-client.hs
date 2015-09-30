6294774637f08d560a3fc77bd8ca2c4811b3d2f4
Simon Meier <simon.meier@erudify.com>
2015-09-30 17:49:32 +0200

    Extract 'serverUrl' from injected 'SERVER-URL.js' script.
    This ensure that the 'cExternalServerUrl' property is actually respected.

--- libs/hs/blaze-react-spa/main-src/dev-mode-client.hs
+++ libs/hs/blaze-react-spa/main-src/dev-mode-client.hs
@@ -19,35 +19,53 @@ import           Control.Monad.Trans.Either   (bimapEitherT)
 
 import qualified Data.Text                    as T
 
+import           GHCJS.Types           (JSString)
+import qualified GHCJS.Foreign         as Foreign
+
+
 import           Servant.API
 import           Servant.Client
 
+------------------------------------------------------------------------------
+-- FFI
+------------------------------------------------------------------------------
+
+foreign import javascript safe
+    "$r = BLAZE_REACT_DEV_MODE_SERVER_URL"
+    js_getServerUrl :: IO JSString
+
+-- | Fetch the server url that was injected via the SERVER-URL.js script.
+getServerUrl :: IO String
+getServerUrl = Foreign.fromJSString <$> js_getServerUrl
+
+
+------------------------------------------------------------------------------
+-- Main
+------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
     -- create logger - obvious comment is obvious (TC)
-    loggerH <- Logger.newStdoutLogger
+    loggerH    <- Logger.newStdoutLogger
+    serverUrl0 <- getServerUrl
+    serverUrl  <- parseBaseUrl serverUrl0
     -- TODO (SM): extract server-url from environment
 
     -- define client application
-    let serverH = Client.Handle
+    let postEvent :<|> getView = client ProxyApi.api serverUrl
+        serverH = Client.Handle
           { Client.hLogger    = loggerH
           , Client.hPostEvent = showErrors . postEvent
           , Client.hGetView   = showErrors . getView
           }
-        clientApp = Client.clientAppFor serverH (T.pack (show serverUrl))
+        clientApp = Client.clientAppFor serverH (T.pack serverUrl0)
 
     -- run client application using React.js
     runApp' (raRender clientApp) (runIORequest <$> raApp clientApp)
   where
-    postEvent :<|> getView = client ProxyApi.api serverUrl
-
-    serverUrl  = BaseUrl Http "localhost" 8081
 
     -- TODO (SM): show the errors once they do not use 'undefined' in the
     -- 'ghcjs-servant-client' package anymore.
     showErrors = bimapEitherT (const "Some ServantError.") id
     -- showErrors = bimapEitherT show id
-
-
 
