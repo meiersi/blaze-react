2cf2b95e48c1e8c3df30bc94650cf64102262f0f
David Waern <david.waern@gmail.com>
2015-10-19 16:51:55 +0200

    Make it easier to try out improved-base branch

--- default.nix
+++ default.nix
@@ -1,7 +1,16 @@
 # Convenient re-exports of the build functions for the individual cabal
 # packages constituting blaze-react.
-{ nixpkgs ? (import <nixpkgs> {}) }:
+{ pkgs ? import <nixpkgs> {} }:
 let
+  # Hardcoding the version of nixpkgs to make it easier to try out
+  # this branch, since it needs a very recent version.
+  # We can remove this later.
+  nixpkgs = import (pkgs.fetchgit {
+    url = "https://github.com/NixOS/nixpkgs.git";
+    rev = "38fa633b3aa7f239de0cb8e7554b1a2539257df6";
+    sha256 = "1lpia92z77ijhp9rxwmpnmn9fk6g0xhgrzh7n85k8c9b7v4x9864";
+  }) {};
+
   # Individual packages
   #####################
 
@@ -62,6 +71,37 @@ let
     blaze-react-demo            = import apps/hs/blaze-react-example;
   };
 
+  shims = nixpkgs.fetchFromGitHub {
+    owner = "ghcjs";
+    repo = "shims";
+    rev = "356acca65861e8f250d569f9e2f3676fd2827e98";
+    sha256 = "166iva95mvagzds3adigr7g6kfxwza25pqdjfn3s6cd2970midx5";
+  };
+
+  ghcjsBoot = nixpkgs.fetchgit {
+    url = git://github.com/ghcjs/ghcjs-boot.git;
+    rev = "ad1db0f6a2ccfd5f9e21f549c0868bded340b0b9";
+    sha256 = "0d3xiw3r7vkb33fbnxn1s6xlvn5q9izq3x5cxq0259g1si3j3shy";
+    fetchSubmodules = true;
+  };
+
+  ghcjs = nixpkgs.haskell.compiler.ghcjs.override { inherit shims ghcjsBoot; };
+
+  ghcjs' = nixpkgs.haskell.lib.overrideCabal ghcjs (oldAttrs: {
+    src = nixpkgs.fetchFromGitHub {
+      owner = "ghcjs";
+      repo = "ghcjs";
+      rev = "eff0443c3fb188997807431fe52b9ce1a89694b2";
+      sha256 = "18q5sdgdq6fgmfbjnzirpl8k1m72b6rdspc992sk6lsyihs5scyx";
+    };
+    preConfigure = ''
+      sed -e "s/lens.*/lens,/" -i ghcjs.cabal
+      sed -e "s/vector.*/vector,/" -i ghcjs.cabal
+    '';
+  });
+
+  ghcjs-packages = nixpkgs.haskell.packages.ghcjs.override { ghc = ghcjs'; };
+
   # Assembled packages for testing and local building
   assembled =
   let
@@ -78,8 +118,8 @@ let
       };
 
   in {
-    ghc   = extendHaskellPackages nixpkgs.pkgs.haskell.packages.ghc7102;
-    ghcjs = extendHaskellPackages nixpkgs.pkgs.haskell.packages.ghcjs;
+    ghc   = extendHaskellPackages nixpkgs.haskell.packages.ghc7102;
+    ghcjs = extendHaskellPackages ghcjs-packages;
   };
 in
 {
